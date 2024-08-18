eee3d14d08dee471885bef3bf7a5c46c8cdf8493
Gaco Razan Kamil <2241720091@student.polinema.ac.id>
2024-08-18 11:37:25 +0700

    NextJS - API Routes & FIrebase DB Connection

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,16 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+type ProductType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
+
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const [products, setProducts] = useState([]);
   const { push } = useRouter();
 
   useEffect(() => {
@@ -11,9 +19,24 @@ const ProductPage = () => {
     }
   });
 
+  useEffect(() => {
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Product Page</h1>
+      {products.map((product: ProductType) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
+          <p>{product.price}</p>
+          <p>{product.size}</p>
+        </div>
+      ))}
     </div>
   );
 };
